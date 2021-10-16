46e27453a770e2dc83e776029f43bc46837f4781
rohitsainier <rohitsainier@gmail.com>
2021-10-16 21:57:18 +0530

    FootervieW added

---
+++ TouchDown/Utility/Constant.swift
@@ -0,0 +1,22 @@
+//
+//  Constant.swift
+//  TouchDown
+//
+//  Created by Rohit Saini on 16/10/21.
+//
+
+import SwiftUI
+
+// DATA
+// COLOR
+
+let colorBackground = Color("ColorBackground")
+let colorGraY = Color(UIColor.systemGray4)
+
+// LAYOUT
+// UX
+// API
+// IMAGE
+// FONT
+// STRING
+// MISC
