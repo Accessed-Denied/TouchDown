9832ff97c034c4e615d86ee2080e232e6b1e377b
sonusainier <rohitsaini@lambdatest.com>
2021-10-16 12:39:48 +0530

    Initial Commit

---
+++ TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by Rohit Saini on 16/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
